d697daf9fdeab311a04a0286e95ec3b404a1df5f
Sergey Pozharitsky <pozhar511@gmail.com>
2021-05-06 22:21:28 +0300

    feat: added coder.js file

---
+++ src/coder.js
@@ -0,0 +1,40 @@
+export function encoded(shift) {
+    return function (chunk, enc, callback) {
+        for (let i = 0; i < chunk.length; i++) {
+            if (chunk[i] > 61 && chunk[i] < 91) {
+                chunk[i] += shift;
+                if (chunk[i] > 90) {
+                    chunk[i] -= 26;
+                }
+            }
+            if (chunk[i] > 96 && chunk[i] < 123) {
+                chunk[i] += +shift;
+                if (chunk[i] > 122) {
+                    chunk[i] -=26;
+                }
+            }
+        }
+        this.push(chunk);
+    };
+}
+
+export function decoded(shift) {
+    return function (chunk, enc, callback) {
+        for (let i = 0; i < chunk.length; i++) {
+          if (chunk[i] > 61 && chunk[i] < 91) {
+            chunk[i] -= +shift;
+            if (chunk[i] < 62) {
+              chunk[i] += 26;
+            }
+          }
+          if (chunk[i] > 96 && chunk[i] < 123) {
+            chunk[i] -= +shift;
+            if (chunk[i] < 97) {
+              chunk[i] += 26;
+            }
+          }
+        }
+        this.push(chunk);
+        callback();
+      };
+}
